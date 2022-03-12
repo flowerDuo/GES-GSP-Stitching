a88cfadc6e2da5f7005e3deaebc9d473ffa8fd27
aalallalalal <aimsunshine@163.com>
2022-03-12 10:07:10 +0800

    change stucture

--- Util/EdgeDetection.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#pragma once
-#include "../Configure.h"
-#include "../model/CropLayer.h"
-
-void edgeDetection(cv::Mat& src, cv::Mat& dst, double threshold);
--- Util/Thin.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-#include "../Configure.h"
-#include <imgproc/types_c.h>
-#include "../Feature/ImageData.h"
-
-
-void thin(Mat srcImage, Mat& dst, double kernalSizeTimes);
-void thinTest(Mat srcImage, Mat& dst, double kernalSizeTimes);
